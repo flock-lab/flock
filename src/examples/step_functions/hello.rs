cc57ccbadb23ac0aff5ddeb504e754dd89ae04dc
Gang Liao <gangliao@cs.umd.edu>
2020-10-29 01:09:57 -0400

    ci: add step function deployment

---
+++ src/examples/step_functions/hello.rs
@@ -0,0 +1,16 @@
+use lambda::{handler_fn, Context};
+use serde_json::{json, Value};
+
+type Error = Box<dyn std::error::Error + Sync + Send + 'static>;
+
+async fn handler(event: Value, _: Context) -> Result<Value, Error> {
+    let message = event["input"].as_str().unwrap();
+    let event = json!({ "input": format!("{} {}", "Hello!", message) });
+    Ok(event)
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    lambda::run(handler_fn(handler)).await?;
+    Ok(())
+}
