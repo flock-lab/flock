28caa47c6056e6c13e144ba19de423733cd63ea9
Gang Liao <gangliao@cs.umd.edu>
2020-11-26 20:53:00 -0500

    doc: update copyright

--- scripts/copyright.py
+++ scripts/copyright.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-# Copyright 2020 UMD Database Group. All rights reserved.
+# Copyright (c) 2020 UMD Database Group. All rights reserved.
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -19,11 +20,14 @@ import subprocess
 import platform
 
 COPYRIGHT = '''
-Copyright 2019 UMD Database Group. All rights reserved.
+Copyright (c) 2020 UMD Database Group. All rights reserved.
+
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
+
 http://www.apache.org/licenses/LICENSE-2.0
+
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
