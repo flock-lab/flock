8125a48a54b16ec4e13946597f0b04dc3d74cd59
Zejun <zejun@Zejuns-MBP.fios-router.home>
2020-12-08 17:41:44 -0500

    op: join

--- src/ops/sample_data_stream.py
+++ src/ops/sample_data_stream.py
@@ -1,3 +1,17 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2020 UMD Database Group. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import json
 import boto3
@@ -33,18 +47,16 @@ for i in range(50):
     if (rnd < 0.5):
         data = json.dumps(getStream1Data())
         print(data)
-        kinesis.put_record(
-            StreamName="stream1",
-            Data=data,
-            PartitionKey="partitionkey")
+        kinesis.put_record(StreamName="stream1",
+                           Data=data,
+                           PartitionKey="partitionkey")
         stream1 += 1
     else:
         data = json.dumps(getStream2Data())
         print(data)
-        kinesis.put_record(
-            StreamName="stream2",
-            Data=data,
-            PartitionKey="partitionkey")
+        kinesis.put_record(StreamName="stream2",
+                           Data=data,
+                           PartitionKey="partitionkey")
         stream2 += 1
 
 # for i in range(10):
