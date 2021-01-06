fcf0f50b1114e02850b8da30b21cc0fd2b62aed4
Gang Liao <gangliao@cs.umd.edu>
2021-01-06 05:09:08 -0500

    example: serialize arrow sql statement

--- examples/sql/plan.rs
+++ examples/sql/plan.rs
@@ -38,6 +38,11 @@ mod tests {
 
     #[tokio::test]
     async fn simple_avg() -> Result<(), Error> {
+        use datafusion::physical_plan::hash_aggregate::HashAggregateExec;
+        use datafusion::physical_plan::memory::MemoryExec;
+        use datafusion::physical_plan::merge::MergeExec;
+        use datafusion::physical_plan::ExecutionPlan;
+
         let schema = Schema::new(vec![Field::new("a", DataType::Int32, false)]);
 
         let batch1 = RecordBatch::try_new(
@@ -58,6 +63,7 @@ mod tests {
         let logical_plan = ctx.create_logical_plan(&sql)?;
         let logical_plan = ctx.optimize(&logical_plan)?;
         let physical_plan = ctx.create_physical_plan(&logical_plan)?;
+        let physical_plan_clone = physical_plan.clone();
 
         let result = collect(physical_plan).await?;
 
@@ -73,6 +79,52 @@ mod tests {
         assert_eq!(values.len(), 1);
         // avg(1,2,3,4,5) = 3.0
         assert_lt!(values.value(0) - 3.0_f64, f64::EPSILON);
+
+        // Seralization
+        let serialized = serde_json::to_string(&physical_plan_clone).unwrap();
+        println!("{}", serialized);
+        assert_eq!(
+            r#"{"execution_plan":"hash_aggregate_exec","mode":"Final","group_expr":[],"aggr_expr":[{"aggregate_expr":"avg","name":"AVG(a)","data_type":"Float64","nullable":true,"expr":{"physical_expr":"column","name":"a"}}],"input":{"execution_plan":"merge_exec","input":{"execution_plan":"hash_aggregate_exec","mode":"Partial","group_expr":[],"aggr_expr":[{"aggregate_expr":"avg","name":"AVG(a)","data_type":"Float64","nullable":true,"expr":{"physical_expr":"column","name":"a"}}],"input":{"execution_plan":"memory_exec","schema":{"fields":[{"name":"a","data_type":"Int32","nullable":false,"dict_id":0,"dict_is_ordered":false}],"metadata":{}},"projection":[0]},"schema":{"fields":[{"name":"AVG(a)[count]","data_type":"UInt64","nullable":true,"dict_id":0,"dict_is_ordered":false},{"name":"AVG(a)[sum]","data_type":"Float64","nullable":true,"dict_id":0,"dict_is_ordered":false}],"metadata":{}}}},"schema":{"fields":[{"name":"AVG(a)","data_type":"Float64","nullable":true,"dict_id":0,"dict_is_ordered":false}],"metadata":{}}}"#,
+            serialized
+        );
+
+        // Deseralization
+        let physical_plan: Arc<dyn ExecutionPlan> = serde_json::from_str(&serialized).unwrap();
+        // Feed recordBatch to MemTable
+        if let Some(hash_aggregate) = physical_plan.as_any().downcast_ref::<HashAggregateExec>() {
+            if let Some(merge) = hash_aggregate.children()[0]
+                .as_any()
+                .downcast_ref::<MergeExec>()
+            {
+                if let Some(hash_aggregate) = merge.children()[0]
+                    .as_any()
+                    .downcast_ref::<HashAggregateExec>()
+                {
+                    if let Some(memory) = hash_aggregate.children()[0]
+                        .as_any()
+                        .downcast_ref::<MemoryExec>()
+                    {
+                        let schema = Schema::new(vec![Field::new("a", DataType::Int32, false)]);
+                        let batch1 = RecordBatch::try_new(
+                            Arc::new(schema.clone()),
+                            vec![Arc::new(Int32Array::from(vec![1, 2, 3]))],
+                        )?;
+                        let batch2 = RecordBatch::try_new(
+                            Arc::new(schema),
+                            vec![Arc::new(Int32Array::from(vec![4, 5]))],
+                        )?;
+
+                        // Workflow
+                        let mut memory = (*memory).clone();
+                        memory.set_partitions(vec![vec![batch1], vec![batch2]]);
+
+                        let workflow = (memory, hash_aggregate, merge);
+                        let _batch_stream = workflow.0.execute(0);
+                    }
+                }
+            }
+        }
+
         Ok(())
     }
 }
