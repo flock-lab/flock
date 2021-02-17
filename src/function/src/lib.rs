9e07561afee892e45fa21beef99f0f87f8409ceb
Gang Liao <gangliao@cs.umd.edu>
2021-02-17 02:27:03 -0500

    feat!: adaptive query processing

---
+++ src/function/src/lib.rs
@@ -0,0 +1,28 @@
+// Copyright (c) 2020-2021, UMD Database Group. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#![warn(missing_docs)]
+// Clippy lints, some should be disabled incrementally
+#![allow(
+    clippy::float_cmp,
+    clippy::module_inception,
+    clippy::new_without_default,
+    clippy::ptr_arg,
+    clippy::type_complexity,
+    clippy::wrong_self_convention,
+    clippy::should_implement_trait
+)]
+#![feature(get_mut_unchecked)]
+
+pub mod executor;
