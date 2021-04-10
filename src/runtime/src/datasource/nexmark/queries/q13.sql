9b413b1817ffb8058ff0394a08f1feb5dab40cc5
Gang Liao <gangliao@cs.umd.edu>
2021-04-10 00:32:57 -0400

    nextmark: sql query 13

---
+++ src/runtime/src/datasource/nexmark/queries/q13.sql
@@ -0,0 +1,24 @@
+-- -------------------------------------------------------------------------------------------------
+-- Query 13: Bounded Side Input Join (Not in original suite)
+-- -------------------------------------------------------------------------------------------------
+-- Joins a stream to a bounded side input, modeling basic stream enrichment.
+-- -------------------------------------------------------------------------------------------------
+
+CREATE TABLE side_input (
+  key BIGINT,
+  `value` VARCHAR
+) WITH (
+  'connector.type' = 'filesystem',
+  'connector.path' = 'file://${FLINK_HOME}/data/side_input.txt',
+  'format.type' = 'csv'
+);
+
+SELECT
+    B.auction,
+    B.bidder,
+    B.price,
+    B.dateTime,
+    S.`value`
+FROM (SELECT *, PROCTIME() as p_time FROM bid) B
+JOIN side_input FOR SYSTEM_TIME AS OF B.p_time AS S
+ON mod(B.auction, 10000) = S.key;
