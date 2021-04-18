ddd87e602e4d1f9e245e8eb1da8c7f581945b8e3
Gang Liao <gangliao@cs.umd.edu>
2021-04-18 16:15:42 -0400

    feat!: support mimalloc (#251)
    * feat!: support mimalloc
    
    * codecov: ignore benchmarks

--- src/function/src/aws/lambda.rs
+++ src/function/src/aws/lambda.rs
@@ -33,6 +33,10 @@ use std::sync::Once;
 #[global_allocator]
 static ALLOC: snmalloc_rs::SnMalloc = snmalloc_rs::SnMalloc;
 
+#[cfg(feature = "mimalloc")]
+#[global_allocator]
+static ALLOC: mimalloc::MiMalloc = mimalloc::MiMalloc;
+
 /// Initializes the lambda function once and only once.
 static INIT: Once = Once::new();
 
