b8fbff1680a51f7722c1ec661162ffb449356c6a
gangliao <gangliao@users.noreply.github.com>
2021-11-27 07:58:32 +0000

    deploy: 348bfa51793fe682e337161b5bfae403c90122cd

--- src/runtime/src/datasource/nexmark/queries/q0.sql
+++
@@ -1,8 +0,0 @@
--- -------------------------------------------------------------------------------------------------
--- Query 0: Pass through (Not in original suite)
--- -------------------------------------------------------------------------------------------------
--- This measures the monitoring overhead of Flock implementation including the source generator.
--- Using `bid` events here, as they are most numerous with default configuration.
--- -------------------------------------------------------------------------------------------------
-
-SELECT * FROM bid;
--- src/test/data/tpch/q6.sql
+++
@@ -1,6 +0,0 @@
-SELECT SUM(l_extendedprice * l_discount) AS revenue
-FROM   lineitem
-WHERE  l_shipdate >= DATE '1994-01-01'
-       AND l_shipdate < DATE '1995-01-01'
-       AND l_discount BETWEEN .06 - 0.01 AND .06 + 0.01
-       AND l_quantity < 24;
