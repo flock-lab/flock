7bff82655a00d121805db566b19e0427b730d7b3
Gang Liao <gangliao@cs.umd.edu>
2022-01-09 20:25:38 +0000

    refactor!: add launcher for different function services

---
+++ flock-function/src/gcp/mod.rs
@@ -0,0 +1,37 @@
+// Copyright (c) 2020-present, UMD Database Group.
+//
+// This program is free software: you can use, redistribute, and/or modify
+// it under the terms of the GNU Affero General Public License, version 3
+// or later ("AGPL"), as published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+//! The `gcp` crate contains the GCP-specific parts of the `flock-function`
+//! library.
+
+use crate::launcher::Launcher;
+use flock::driver::funcgen::function::QueryFlow;
+use flock::error::Result;
+
+/// GCPLauncher defines the interface for deploying and executing
+/// queries on GCP Functions.  
+pub struct GCPLauncher {}
+
+impl Launcher for GCPLauncher {
+    fn new(_query: &QueryFlow) -> Self {
+        GCPLauncher {}
+    }
+
+    fn deploy(&self) -> Result<()> {
+        unimplemented!();
+    }
+
+    fn execute(&self) -> Result<()> {
+        unimplemented!();
+    }
+}
